167a679ffc11c0697792c2908c3c0fae4951167b
mohamed yasser <yassermohamed7399@gmail.com>
2024-03-24 23:14:28 +0200

    initial commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userCotroller = require('./../controler/userController')
-router.route('/').get(userCotroller.getUser)
+router.route('/')
+.get(userCotroller.getUser)
 module.exports = router

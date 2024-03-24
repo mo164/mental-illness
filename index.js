f0cb7144aefaf85ea0c355bce6b78e5589a63cd5
mohamed yasser <yassermohamed7399@gmail.com>
2024-03-25 00:13:49 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes')
+const compressions = require('compressions');
 
  app.use('/api/v1/users',userRoutes);
 module.exports = app

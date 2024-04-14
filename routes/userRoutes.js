12680dfd68cb1aa0bec6c4761ae0ff3c862fec03
mohamed yasser <yassermohamed7399@gmail.com>
2024-04-14 21:50:14 +0200

    commited now

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 //const userModel = require('./../models/userModel')
 const authControllers = require('./../controllers/authControllers')
+const userControllers = require('./../controllers/userControllers')
 const router = express.Router();
+router.get('/', userControllers.getAll)
 router.post('/signUp', authControllers.signUp);
 
 module.exports = router

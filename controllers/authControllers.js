6863d007be6c138f1c953e1a80400792e2e84757
mohamed yasser <yassermohamed7399@gmail.com>
2024-04-14 23:39:13 +0200

    commit

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,4 @@
-const User = require('./../models/userModel')
+const User = require('../models/userModel')
 //const validator = require('./../validator')
 exports.signUp = async (req,res,next) =>{
     const newUser = await User.create({

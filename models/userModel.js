d2dd4f9456830d7574f55e9f6855ea7a5d416a14
mohamed yasser <yassermohamed7399@gmail.com>
2024-05-07 01:48:56 +0200

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema({
   role:{
     type: String,
     enum: ['admin','user'],
-    default: 'user'
+    default: 'user'            
   },
   passwordConfirm: {
     type: String,

88178b4667f4a4dba76dc8eb4fba43d24d075749
mohamed yasser <yassermohamed7399@gmail.com>
2024-05-09 00:55:45 +0200

    commit

--- routes/getAllDoctors.js
+++ routes/getAllDoctors.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+//
 const doctorController = require('./../controllers/doctorController')
 router.get('/', doctorController.getAll)
 router
